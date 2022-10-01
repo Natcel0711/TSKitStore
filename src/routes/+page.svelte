149c9388a4ebbe7e7d2d162948354b694c161aa4
Natcel <natcelnieves@gmail.com>
2022-10-01 13:28:22 -0400

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,22 @@
+<script lang="ts">
+    import { page } from '$app/stores'
+</script>
+
+{#if $page.data.user}
+<div class="card lg:card-side bg-base-100 shadow-xl">
+    <figure><img src="https://placeimg.com/400/400/arch" alt="Album"/></figure>
+    <div class="card-body">
+      <h2 class="card-title">New album is released!</h2>
+      <p>Click the button to listen on Spotiwhy app.</p>
+      <div class="card-actions justify-end">
+        <button class="btn btn-primary">Listen</button>
+      </div>
+    </div>
+  </div>
+  {/if}
+
+
+{#if !$page.data.user}
 <div class="hero min-h-screen bg-base-200">
     <div class="hero-content text-center">
       <div class="max-w-md">
@@ -7,3 +26,4 @@
       </div>
     </div>
   </div>
+{/if}
